daa8027e933fc2818b1a4958afc2d5ddbfb26814
gavincraig <gcraig197@gmail.com>
2023-10-25 10:27:29 +0100

    current user context and comment styling

--- comments-client/src/App.tsx
+++ comments-client/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Comment as CommentType, User } from "./types";
 import { Comment } from "./components/Comment";
+import { CurrentUserContext } from "./context/CurrentUserContext";
 
 function App() {
   const [comments, setComments] = useState<CommentType[] | []>([]);
@@ -17,15 +18,17 @@ function App() {
 
   return (
     <main className="bg-very-light-gray min-h-screen px-4 py-8">
-      {comments.length > 0 && (
-        <ul className="flex flex-col gap-4">
-          {comments.map((comment) => (
-            <li key={comment.id}>
-              <Comment comment={comment} />
-            </li>
-          ))}
-        </ul>
-      )}
+      <CurrentUserContext.Provider value={currentUser}>
+        {comments.length > 0 && (
+          <ul className="flex flex-col gap-4">
+            {comments.map((comment) => (
+              <li key={comment.id}>
+                <Comment comment={comment} />
+              </li>
+            ))}
+          </ul>
+        )}
+      </CurrentUserContext.Provider>
     </main>
   );
 }
--- comments-client/src/components/Comment/Comment.tsx
+++ comments-client/src/components/Comment/Comment.tsx
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Comment } from "../../types";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
 
 type CommentProps = {
   comment: Comment;
+  isReply?: boolean;
 };
 
 // http://localhost:5173/src/assets/images/avatars/image-amyrobson.png
 
-const Comment = ({ comment }: CommentProps) => {
-    
+const Comment = ({ comment, isReply }: CommentProps) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const isOwnComment = comment.user.username === currentUser.username;
   return (
     <>
       <div className="flex flex-col bg-white rounded-lg p-4 gap-4">
-          <div className="flex gap-4">
-            <img src={`${comment.user.image.webp}`} width={32} height={32} />
-            <span>{comment.user.username}</span>
-            <span>{comment.createdAt}</span>
-          </div>
-          <p>{comment.content}</p>
+        <div className="flex gap-4">
+          <img src={`${comment.user.image.webp}`} width={32} height={32} />
+          <strong>
+            {comment.user.username}
+            {isOwnComment && <span className="bg-moderate-blue text-white text-xs font-medium rounded-sm px-1.5 py-1 ml-2">you</span>}
+            </strong>
+          <span>{comment.createdAt}</span>
+        </div>
+        <p>
+          {isReply && (
+            <span className="text-moderate-blue font-medium">{`@${comment.replyingTo} `}</span>
+          )}
+          {comment.content}
+        </p>
         <div className="flex justify-between">
           <div>{comment.score}</div>
           <button>reply</button>
@@ -27,7 +39,7 @@ const Comment = ({ comment }: CommentProps) => {
         <ul className="flex flex-col pl-4 mt-4 gap-4 border-l-2 border-light-gray">
           {comment.replies.map((reply) => (
             <li key={reply.id}>
-              <Comment comment={reply} />
+              <Comment comment={reply} isReply />
             </li>
           ))}
         </ul>
